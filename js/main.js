d2cd2a1fb33583561589609ef4e3aff414c1d569
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-03 21:12:03 +0000

    finished writing js code for dynamic option settings and edited html elements accordingly

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(()=>{
+jQuery(function(){
     /*TO DO:
       -stack options:
         - when nonstack:
@@ -65,8 +65,11 @@ $(document).ready(()=>{
     },
 
     bColor:{
-      sync:true,
-      colors:'#000000'
+      allSync:true,
+      stackSync:false,
+      allColor:'#ff0000',
+      colors:{},
+      stackColors: {}
     },
 
     title:{
@@ -87,7 +90,8 @@ $(document).ready(()=>{
       ycolor:'#000000',
       yfont:'Arial',
       ysize:10,
-      yangle: 0
+      yangle: 0,
+      numticks: 0
     },
 
     legend:{
@@ -101,9 +105,31 @@ $(document).ready(()=>{
   };
 
   //TYPE STTINGS
+  //TO DO: IF DATA AND LABELS ARE ALREADY PRESENT AND TYPE CHANGES, RESET DATA AND LABELS AND PROMPT USER TO RE-ENTER
+  //CLEAR BAR COLOR SETTINGS: REMOVE ALL .barColor elements, remove saved colors in object, and check colorsync off.
+
   $('input[type=radio][name=type]').on('change', ()=>{
     let value = $('input[name=type]:checked').val();
     let $slLabel, $slText, $slInst, $slButton;
+
+    if(data.length !== 0 || options.label.barLabel.length !== 0){
+      data = [];
+      options.label.barLabel = [];
+      options.label.stackLabel = [];
+      options.bColor.allSync = true;
+      options.bColor.stackSync = false;
+      options.bColor.colors = {};
+      options.bColor.stackColors = {};
+
+      $('#data').val('');
+      $('#dataLabel').val('');
+      $('#stackLabel').val('');
+      $('.barColor').remove();
+      $('#barColorSync').prop('checked', true);
+
+      alert("You have changed the graph's style: please re-enter your data and labels");
+    }
+
     switch(value){
       case "nonstack":
         options.type = 0;
@@ -137,13 +163,15 @@ $(document).ready(()=>{
     }
   });
 
-  //GLOBAL FONT SETTINGS
-  $('input[type=checkbox][name=fontSync]').on('change', ()=>{
+  //GLOBAL FONT SETTINGS -> this can be done efficietly using classes....
+  $('input[type=checkbox][name=fontSync]').on('change', function(){
     let boxChecked = $('input[type=checkbox][name=fontSync]').is(":checked");
     if(boxChecked){
       options.font.sync = true;
       let globalFont = $("#docFont").children("option:selected").val();
       let globalColor = $("#fontColor").val();
+      options.font.color = globalColor;
+      options.font.type = globalFont;
       options.title.font = globalFont;
       options.title.color = globalColor;
       options.axis.xfont = globalFont;
@@ -167,7 +195,6 @@ $(document).ready(()=>{
     }
     else{
       options.font.sync = false;
-
       options.title.font = $("#titleFont").children("option:selected").val();
       options.title.color = $("#titlecolor").val();
       options.axis.xfont = $("#xfont").children("option:selected").val();
@@ -217,13 +244,17 @@ $(document).ready(()=>{
     options.title.font = $('#titleFont').children("options:selected").val();
   });
   $('#titlecolor').on('change', ()=>{
-    options.title.font = $('#titlecolor').val();
+    options.title.color = $('#titlecolor').val();
   });
-  $('#titlesize').on('keyup mouseup', ()=>{
-    let num = $('#titlesize').val();
-    if(num>0 && num <=501) options.title.size = num;
-    else alert('Please enter a value between the min and the max font size');
-    console.log(options.title.size);
+  $('#titlesize').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num <=max) options.title.size = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
   });
 
   //DATA SETTINGS
@@ -325,16 +356,267 @@ $(document).ready(()=>{
   });
 
   //SIZE SETTINGS
+  $(document).on('keyup mouseup', '#width', function(){
+    let min = $('#width').prop('min');
+    let max = $('#width').prop('max');
+    let num = $('#width').val();
+    if(num>=min && num <=max) options.dim.width = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+
+  $(document).on('keyup mouseup', '#height', function(){
+    let min = $('#height').prop('min');
+    let max = $('#height').prop('max');
+    let num = $('#height').val();
+    if(num>=min && num <=max) options.dim.height = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
 
   //BAR COLOR SETTINGS
+  $(document).on('change', '#barColorSync', function(){
+    let checked = $('#barColorSync').is(":checked");
+    if(data.length === 0 || options.label.barLabel.length === 0){
+      $(this).prop('checked', !checked);
+      alert("Please enter your data and label first before configuring the colors");
+    }
+    else if(checked){
+      $('#allBar').prop('disabled', false);
+      if($('.barColor').length) $('.barColor').remove();
+      options.bColor.allSync = true;
+      options.bColor.allColor = $('#allBar').val();
+    }
+    else{
+      options.bColor.allSync = false;
+      options.bColor.stackSync = false;
+      $('#allBar').prop('disabled', true);
+      let currColor;
+      let savedColors = options.bColor.colors;
+
+      if(options.type === 0){
+        let bLabel = options.label.barLabel;
+        for(let i=0 ; i<bLabel.length; i++){
+          currColor = bLabel[i] in savedColors ? savedColors[bLabel[i]] : "#ff0000";
+          $bcLabel = $(`<label for="${bLabel[i]}" class="barColor">Select the color for ${bLabel[i]}:</label>`);
+          $bcSelect = $(`<input type="color" id="${bLabel[i]}" name="${bLabel[i]}" class="barColor" value=${currColor}>`);
+          savedColors[bLabel[i]] = currColor;
+
+          $("#barColorSetting").append($bcLabel, $bcSelect);
+        }
+      }
+
+      else if(options.type === 1){
+        $stackSync = $(`<input type="checkbox" id="stackColorSync" name="stackColorSync" class="barColor" value="stackColorSync">`);
+        $stackSyncLabel = $(`<label for="barColorSync" class="barColor">synchronize stack colors?</label>`);
+        $('#allBar').after($stackSync, $stackSyncLabel);
+
+        options.bColor.stackSync = false;
+        let bLabel = options.label.barLabel;
+        let sLabel = options.label.stackLabel;
+        for(let i=0 ; i<bLabel.length ; i++){
+          for(let j=0 ; j<sLabel.length ; j++){
+            let name = `${bLabel[i]}-${sLabel[j]}`;
+            currColor = name in savedColors ? savedColors[name] : "#ff0000";
+            $bcLabel = $(`<label for="${name}" class="barColor uniStack">Select the color for ${name}:</label>`);
+            $bcSelect = $(`<input type="color" id="${name}" name="${name}" class="barColor uniStack" value=${currColor}>`);
+            savedColors[name] = currColor;
+
+            $("#barColorSetting").append($bcLabel, $bcSelect);
+          }
+        }
+      }
+
+      else{
+        let bLabel = options.label.barLabel;
+        let sLabel = options.label.stackLabel;
+        for(let i=0 ; i<bLabel.length ; i++){
+          let stack = sLabel[i];
+          let currBar = bLabel[i];
+          for(let j=0 ; j<stack.length ; j++){
+            let name = `${currBar}-${stack[j]}`;
+            currColor = name in savedColors ? savedColors[name] : "#ff0000";
+            $bcLabel = $(`<label for="${name}" class="barColor">Select the color for ${name}:</label>`);
+            $bcSelect = $(`<input type="color" id="${name}" name="${name}" class="barColor" value=${currColor}>`);
+            savedColors[name] = currColor;
+
+            $("#barColorSetting").append($bcLabel, $bcSelect);
+          }
+        }
+      }
+    }
+  });
+
+  $(document).on('change', '#stackColorSync', () => {
+    let checked = $('#stackColorSync').is(":checked");
+    if($(".barColor.uniStack").length !== 0) $(".barColor.uniStack").remove();
+
+    let bLabel = options.label.barLabel;
+    let sLabel = options.label.stackLabel;
+    let currColor;
+    let unsyncColors = options.bColor.colors;
+    let syncColors = options.bColor.stackColors;
+
+    if(data.length ===0 || bLabel.length === 0) alert("Please enter your data and label first");
+    else if(checked){
+      options.bColor.stackSync = true;
+      for(let i=0; i<sLabel.length ; i++){
+        currColor = sLabel[i] in syncColors ? syncColors[sLabel[i]] : "#ff0000";
+        $bcLabel = $(`<label for="${sLabel[i]}" class="barColor uniStack sync">Select the color for ${sLabel[i]}:</label>`);
+        $bcSelect = $(`<input type="color" id="${sLabel[i]}" name="${sLabel[i]}" class="barColor uniStack sync" value=${currColor}>`);
+        syncColors[sLabel[i]] = currColor;
+
+        $("#barColorSetting").append($bcLabel, $bcSelect);
+      }
+    }
+    else{
+      options.bColor.stackSync = false;
+      for(let i=0 ; i<bLabel.length ; i++){
+        for(let j=0 ; j<sLabel.length ; j++){
+          let name = `${bLabel[i]}-${sLabel[j]}`;
+          currColor = name in unsyncColors ? unsyncColors[name] : "#ff0000";
+          $bcLabel = $(`<label for="${name}" class="barColor uniStack">Select the color for ${name}:</label>`);
+          $bcSelect = $(`<input type="color" id="${name}" name="${name}" class="barColor uniStack" value=${currColor}>`);
+          unsyncColors[name] = currColor;
+
+          $("#barColorSetting").append($bcLabel, $bcSelect);
+        }
+      }
+    }
+  });
+
+  $(document).on('change', '#allBar', ()=>{
+    options.bColor.allColor = $('#allBar').val();
+  });
+  $(document).on('change', 'input[type="color"].barColor',function (){
+    let name = $(this).attr('id');
+    if($(this).hasClass("sync")) options.bColor.stackColors[name] = $(this).val();
+    else options.bColor.colors[name] = $(this).val();
+  });
 
   //AXES SETTINGS
+  $('#showx').on('change', function(){
+    if($(this).is(':checked')) options.axis.xshow = true;
+    else options.axis.xshow = false;
+  });
+  $('#xsize').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.axis.xsize = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+  $('#xcolor').on('change', function(){
+    options.axis.xcolor = $(this).val();
+  });
+  $('#xfont').on('change', function(){
+    options.axis.xfont = $(this).children("option:selected").val();
+  });
+  $('#xangle').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.axis.xangle = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+
+  $('#showy').on('change', function(){
+    if($(this).is(':checked')) options.axis.yshow = true;
+    else options.axis.yshow = false;
+  });
+  $('#ysize').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.axis.ysize = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+  $('#ycolor').on('change', function(){
+    options.axis.ycolor = $(this).val();
+  });
+  $('#yfont').on('change', function(){
+    options.axis.yfont = $(this).children("option:selected").val();
+  });
+  $('#yangle').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.axis.yangle = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+  $('#numTicks').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let num = Number($(this).val());
+    if(num>=min) options.axis.numticks = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a positive value`);
+    }
+  });
 
   //LEGEND SETTINGS
+  $('#showleg').on('change', function(){
+    if($(this).is(':checked')) options.legend.show = true;
+    else options.legend.show = false;
+  });
+  $('#legWidth').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.legend.width = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+  $('#legHeight').on('keyup mouseup', function(){
+    let min = Number($(this).prop('min'));
+    let max = Number($(this).prop('max'));
+    let num = Number($(this).val());
+    if(num>=min && num<=max) options.legend.height = num;
+    else{
+      $(this).val('');
+      alert(`Please enter a value between ${min} and ${max}`);
+    }
+  });
+  $("#legcolor").on('change', function(){
+    options.legend.color = $(this).val();
+  });
+  $('#legendFont').on('change', function(){
+    options.legend.font = $(this).children("option:selected").val();
+  });
+  $('#legendLoc').on('change,', function(){
+    options.legend.loc = $(this).children("option:selected").val();
+  });
+
+  //DRAW GRAPH
+  $(document).on('click', '#refreshGraph', function(){
+    console.log(options);
+  });
+
+
+
+
 
   //HELPER FUNCTIONS
-
   const checkSpecialChar = text =>{
     return (text.includes('[') || text.includes('\\') || text.includes('$') || text.includes('.') || text.includes('|') || text.includes('?') || text.includes('*') || text.includes('+') || text.includes('(') || text.includes(')') || text.includes('^'));
   };
+
 });
